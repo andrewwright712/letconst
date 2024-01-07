ac2599f9a7a434d9e32177eb39b1d46508b6da5d
andrewwright712 <150976373+andrewwright712@users.noreply.github.com>
2024-01-07 17:36:11 -0500

    Add files via upload

---
+++ let-const.js
@@ -0,0 +1,11 @@
+const PI = 3.14;
+PI = 42; // stop me from doing this!
+
+
+Var and let are different because var is saves as a child of the window. Let is not.
+
+The differences between var and const are var can be redeclared, const can not.
+
+The differences between let and const are the same as the question above.
+
+Hoisting means your browser will declare empty variables first even if it is not in order.
